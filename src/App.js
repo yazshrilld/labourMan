a45692da73e3114b4ff999b8af1d1dd0531e4522
yazshrilld <yazshrilld@gmail.com>
2022-11-28 12:32:38 +0100

    added a new starter file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Main from './main/Main';
+
+function App() {
+  return (
+   <>
+    <div className='bg-slate-800text-black text-3xl'>
+    Hello, Yazid from dev branch!
+    </div>
+    <Main />
+   </>
+  );
+}
+
+export default App;
