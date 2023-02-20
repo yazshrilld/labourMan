313b3f5752a92592bb0de1e08f8f76c18b7a87ec
yazshrilld <yazshrilld@gmail.com>
2023-02-20 08:18:49 +0100

    added a new file

--- src/components/Locate/LocateIcon.jsx
+++ src/components/Locate/LocateIcon.jsx
@@ -9,7 +9,7 @@ const LocateIcon = () => {
                 name="" 
                 id="" 
                 placeholder='within 25 kilometers'
-                className='pr-14 pl-10 py-2 rounded-md font-semibold placeholder-gray-500 text-black border-none bg-slate-300 focus:border-none'
+                className='pr-14 pl-10 py-2 rounded-md font-semibold placeholder-gray-500 text-black font border-none bg-slate-300 focus:border-none'
             />
             <div>
             <svg className='absolute -ml-8 top-2.5' width="19" height="19" viewBox="0 0 19 19" xmlns="http://www.w3.org/2000/svg">
